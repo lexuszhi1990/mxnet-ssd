2119c04214fac3cd09a0d6535f3b765730077b79
david_fu <gandor@qq.com>
2018-03-21 14:41:34 +0800

    update params

--- tools/prepare_bike.sh
+++ tools/prepare_bike.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 python3.6 $DIR/prepare_dataset.py --dataset bike --set train --target /home/fulingzhi/workspace/mxnet-ssd-bike/data/bike/train-bike.lst  --root /home/fulingzhi/workspace/mxnet-ssd-bike/data/bike --class-names bike,ebike,irrelevant
+python3.6 $DIR/prepare_dataset.py --dataset bike --set train --target /home/fulingzhi/workspace/mxnet-ssd-bike/data/bike/val-bike.lst  --root /home/fulingzhi/workspace/mxnet-ssd-bike/data/bike --class-names bike,ebike,irrelevant
