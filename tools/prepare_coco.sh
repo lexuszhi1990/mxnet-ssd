2646b995bd5d90437a4ff751012f245675e90c91
david_fu <gandor@qq.com>
2018-03-21 10:09:48 +0800

    update bash

--- tools/prepare_coco.sh
+++ tools/prepare_coco.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-python $DIR/prepare_dataset.py --dataset coco --set train2017 --target $DIR/../data/pedestrian/train.lst  --root $DIR/../data/coco
-python $DIR/prepare_dataset.py --dataset coco --set val2017 --target $DIR/../data/pedestrian/val.lst --shuffle False --root $DIR/../data/coco
+python3.6 $DIR/prepare_dataset.py --dataset coco --set train2017 --target /mnt/gf_mnt/datasets/coco_bike_person/train-bike-total.lst  --root /mnt/gf_mnt/datasets/cocoapi
+python3.6 $DIR/prepare_dataset.py --dataset coco --set val2017 --target /mnt/gf_mnt/datasets/coco_bike_person/val-bike-total.lst --shuffle False --root /mnt/gf_mnt/datasets/cocoapi
